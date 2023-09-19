fc54e1e93cb803ff75233e9409dd7e666e201203
KarolinyDaiana <karoliny.cieply@gmail.com>
2023-09-18 21:21:17 -0300

    a

---
+++ 2/vite-project/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Counter from "./lib/Counter.svelte";
+
+  let src = '/src/assets/picole.png';
+
+</script>
+
+<main>
+
+  <h1>VOTAÇÃO</h1>
+
+  <img src={src} alt="Picolé"/>
+
+  <h2>Volta picolé da WEG?</h2>
+
+  <div id="contadores">
+    <Counter />
+  </div>
+    
+</main>
+
+<style>
+
+  img {
+    width: 30%;
+  }
+</style>
---
+++ 2/vite-project/src/lib/Counter.svelte
@@ -0,0 +1,20 @@
+<script>
+  let countSim = 0
+  const mais = () => {
+    countSim++;
+  }
+
+  let countNao = 0
+  const menos = () => {
+    countNao++;
+  }
+
+</script>
+
+<button on:click={mais}>
+  SIM -> {countSim}
+</button>
+
+<button on:click={menos}>
+  NÃO -> {countNao}
+</button>
